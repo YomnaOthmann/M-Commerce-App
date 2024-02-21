72adb44ba0d261e87a95104cc22692afc0a18704
YomnaOthmann <yomnaothmann@gmail.com>
2024-02-21 11:08:48 +0200

    finished network manager and network reachability classes in network folder

--- Shopify/Network/NetworkManager.swift
+++ Shopify/Network/NetworkManager.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Alamofire
+
+protocol NetworkManagerProtocol{
+    func fetch<T: Codable>(url: String, type: T.Type, completionHandler: @escaping (T?)->Void)
+}
+class NetworkManager : NetworkManagerProtocol{
+    func fetch<T: Codable>(url: String, type: T.Type, completionHandler: @escaping (T?)->Void){
+        
+        let url = URL(string: url)
+        guard let url = url else{
+            completionHandler(nil)
+            return
+        }
+        
+        AF.request(url).response { data in
+            guard let data = data.data else{
+                completionHandler(nil)
+                return
+            }
+            
+            do{
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completionHandler(result)
+                
+            }
+            catch let error {
+                
+                completionHandler(nil)
+                print(String(describing: error))
+                print("error in decoding..")
+            }
+            
+        }
+        
+    }
+}
--- Shopify/Network/NetworkReachability.swift
+++ Shopify/Network/NetworkReachability.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Network
+
+class NetworkReachability{
+    
+    static let networkReachability = NetworkReachability()
+    
+    var monitor = NWPathMonitor()
+    
+    var monitorQueue = DispatchQueue(label: "network")
+    var networkStatus : Bool = false
+
+    private init(){
+        monitor.start(queue: monitorQueue)
+        
+        monitor.pathUpdateHandler = {[weak self] path in
+            
+            self?.networkStatus = (path.status == .satisfied)
+            
+        }
+  
+    }
+}
