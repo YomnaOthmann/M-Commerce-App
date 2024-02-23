33cee88d75e76b135980b91d03328353103ece21
YomnaOthmann <yomnaothmann@gmail.com>
2024-02-23 02:54:01 +0200

    added products model

--- Shopify/Utility/API/APIHandler.swift
+++ Shopify/Utility/API/APIHandler.swift
@@ -9,13 +9,13 @@ import Foundation
 
 class APIHandler{
     
-    static let baseUrl : String = "http://e68611fa195924ce2d11aa1909193f1b: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@q2-23-24-ios-sv-team1.myshopify.com/admin/api/2024-01"
+    static let baseUrl : String = "https://e68611fa195924ce2d11aa1909193f1b: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@q2-23-24-ios-sv-team1.myshopify.com/admin/api/2024-01"
 
     enum APIEndPoints :String{
         case products = "/products"
         case orders = "/orders"
         case priceRule = "/price_rules"
-        case discountCode = "discount_codes"
+        case discountCode = "/discount_codes"
         case count = "/count"
         case collections = "/custom_collections"
         case customers = "/customers"
