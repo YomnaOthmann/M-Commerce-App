567909d2eb65b20d10d2fef2f15b433c621f00cf
YomnaOthmann <yomnaothmann@gmail.com>
2024-02-26 17:58:44 +0200

    added missed files

---
+++ Shopify/Utility/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  Shopify
+//
+//  Created by Mac on 24/02/2024.
+//
+
+import Foundation
+import UIKit
+
+extension UISearchBar {
+    func setTextFieldColor(_ color: UIColor) {
+        for subView in self.subviews {
+            for subSubView in subView.subviews {
+                let view = subSubView as? UITextInputTraits
+                if view != nil {
+                    let textField = view as? UITextField
+                    textField?.backgroundColor = color
+                    break
+                }
+            }
+        }
+    }
+}
