0de98957d8a530a9e917175f851745e1184cd40e
YomnaOthmann <yomnaothmann@gmail.com>
2024-02-27 00:59:54 +0200

    added actions for navigation and me screen view model

--- Shopify/Modules/MeScreen/View/MeScreenViewController.swift
+++ Shopify/Modules/MeScreen/View/MeScreenViewController.swift
@@ -14,7 +14,7 @@ class MeScreenViewController: UIViewController {
     @IBOutlet weak var userView: UIView!
     @IBOutlet weak var userMail: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    
+    var user:Customer?
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
@@ -22,16 +22,22 @@ class MeScreenViewController: UIViewController {
         userView.layer.cornerRadius = 20
         userName.text = "Yomna Othman"
         userMail.text = "yomnaothmann@gmail.com"
-        // Do any additional setup after loading the view.
     }
     
-    @IBAction func goToWhishlist(_ sender: Any) {
+    @IBAction func goToWishlist(_ sender: Any) {
+        //TODO: navigate to wishlist
     }
     
     @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true)
     }
     
     @IBAction func goToCart(_ sender: Any) {
+        let cartStoryboard = UIStoryboard(name: "ShoppingBag", bundle: nil)
+        let cartVC = cartStoryboard.instantiateViewController(withIdentifier: "") as! ShoppingBagViewController
+        cartVC.modalPresentationStyle = .fullScreen
+        self.present(cartVC, animated: true)
+        
     }
 }
 extension MeScreenViewController : UITableViewDelegate, UITableViewDataSource{
@@ -59,11 +65,17 @@ extension MeScreenViewController : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row{
         case 0:
+            let ordersStoryboard = UIStoryboard(name: "OrdersScreen", bundle: nil)
+            let ordersVC = ordersStoryboard.instantiateViewController(withIdentifier: "orders") 
+            ordersVC.modalPresentationStyle = .fullScreen
+            self.present(ordersVC, animated: true)
             //TODO: Navigate to Orders Screen
-            print("")
         default:
             // TODO: Navigate to Settings Screen
-            print("")
+            let settingsStoryboard = UIStoryboard(name: "Settings", bundle: nil)
+            let settingsVC = settingsStoryboard.instantiateViewController(withIdentifier: "")
+            settingsVC.modalPresentationStyle = .fullScreen
+            self.present(settingsVC, animated: true)
         }
     }
     
--- Shopify/Modules/MeScreen/View/NotLoggedMeScreenViewController.swift
+++ Shopify/Modules/MeScreen/View/NotLoggedMeScreenViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 class NotLoggedMeScreenViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         
     }
     
@@ -20,7 +20,7 @@ class NotLoggedMeScreenViewController: UIViewController {
     }
     
     @IBAction func goToCart(_ sender: Any) {
-        print("pp")
+        
     }
     
     @IBAction func goToWishlist(_ sender: Any) {
@@ -30,14 +30,5 @@ class NotLoggedMeScreenViewController: UIViewController {
     @IBAction func back(_ sender: Any) {
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
